62a44bb4c4281cfacd7f47440a6134719cfae145
ParkYoungDuk <dydejrdms@naver.com>
2022-12-15 14:58:48 +0900

    1차원 배열 사용법

---
+++ src/chapter05/ArrayTest1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter05;
+
+public class ArrayTest1 {
+    public static void main(String[] args) {
+        int[] subject = {90,80,60,70,93,100,50,66};
+        
+        int total = 0;
+        for(int i = 0 ; i < subject.length ; i++){
+            total += subject[i];
+        }
+        
+        System.out.println(total);
+        
+    }
+}
