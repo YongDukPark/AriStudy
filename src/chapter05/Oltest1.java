774240616ffe3e387226f2ac5ff67375c2b7eafd
ParkYoungDuk <dydejrdms@naver.com>
2022-12-15 13:13:54 +0900

    챕터5 추가

---
+++ src/chapter05/Oltest1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter05;
+
+/**
+ *
+ * @author dydej
+ */
+
+class OverLoding1{
+    void addition(int a, int b){
+        int sum = a + b;
+        System.out.println(sum);
+    }
+    
+    void addtion(float a, float b){
+        float sum = a+b;
+        System.out.println(sum);
+    }
+    
+    void addition(double a, double b){
+        double sum = a+b;
+        System.out.println(sum);
+    }
+    
+    void addition(String a, String b){
+        String sum = a+b;
+        System.out.println(sum);
+    }
+}
+
+
+public class Oltest1 {
+    public static void main(String[] args) {
+        OverLoding1 olt1 = new OverLoding1();
+        
+        olt1.addition(8, 9);
+        olt1.addition(4.1F, 5.5F);
+        olt1.addition(6.29, 10.26);
+        olt1.addition("ok","yes");
+    }
+}
---
+++ src/chapter05/TestClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter05;
+
+import java.util.Scanner;
+
+public class TestClass {
+    public static void main(String[] args) {
+        //안녕 여기에 명령문을 넣어줘 참고로 나 main 메서드는 실행 클래스라고도 하며
+        //메모리에 제일먼저 로드되어 실행된단다 ㅎㅎ 내가 먼저야 너도 예외는 아니고ㅎ
+        
+        
+    }
+}
