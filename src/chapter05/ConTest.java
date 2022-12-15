41923e9a6e4b06005a90ca106d114ea423f977c5
ParkYoungDuk <dydejrdms@naver.com>
2022-12-15 13:41:25 +0900

    생성자 공부2 여러가지 방식으로 사용 하는한 예제

--- src/chapter05/ConTest.java
+++ src/chapter05/ConTest.java
@@ -20,20 +20,27 @@ class Person{
         month = arg_month;
         day = arg_day;
     }
+    Person(){
+        
+    }
 }
-/**
- *
- * @author dydej
- */
+
 public class ConTest {
     public static void main(String[] args) {
         //순서에 맞춰서 생성자 매개변수에 들어감
         Person human = new Person("이병재", 2000, 8, 15);
-        
+        Person human2 = new Person();
+        human2.name = "박용덕";
+        human2.year = 1996;
+        human2.month = 9;
+        human2.day = 24;
+                
         //위에 객체로 값을 Person 클래스의 생성자에 값을 넣고 거기서 맨위 String name; 이런식으로
         //선언해준 곳으로 들어간다.
         //그래서 밑에 human.name을 출력했을경우 위에 생성한 객체의 값이 나온다.
         System.out.println(human.name);
         System.out.println(human.year + "년" + human.month + "월" + human.day + "일생");
+        System.out.println(human2.name);
+        System.out.println(human2.year + "년" + human2.month + "월" + human2.day + "일생");
     }
 }
