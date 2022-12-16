ee2a0d03eff3a09a279f484f77bca6149ffc2527
ParkYoungDuk <dydejrdms@naver.com>
2022-12-16 10:57:04 +0900

    Setter Getter 간단히

---
+++ src/chapter06/settergetter.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter06;
+
+class testtest{
+    private String testtest;
+    
+    public void setTest(String testtest){
+        this.testtest = testtest;
+    }
+    
+    public String getTest(){
+        return testtest;
+    }
+}
+
+
+public class settergetter {
+    public static void main(String[] args) {
+        testtest tt = new testtest();
+        tt.setTest("으악 오늘 쉬는날이었다니");
+        
+        System.out.println(tt.getTest());
+        
+    }
+}
