60aad37f314bda584e8ae203805aac4313e661dc
ParkYoungDuk <dydejrdms@naver.com>
2022-12-21 10:17:39 +0900

    join 내용 수정 및 추가

--- src/chapter09/Th6_join.java
+++ src/chapter09/Th6_join.java
@@ -9,14 +9,20 @@ public class Th6_join {
         
         t1.start();
         try{
+            //여기서 join method를 넣지 않고 실행하게 되면 중간에
+            //t2 start의 join이 이 t1의 쓰레드를 block으로 만들어
+            //자신이 모든 실행을 끝내기 전까지 출력을 못한다.
             t1.join();
         } catch (InterruptedException ie){
             System.out.println(ie.toString());
         }
         
         t2.start();
-        
         try {
+            //그럼 여기서 join이 눈에 띄는데
+            //위에서 join이 있어 위에꺼가 다 실행되고 밑에꺼가 실행된다 만약 이후에 새로운
+            //쓰레드가 있다면 그건 이부분이 끝나고 실행될거다.
+            //**반드시 실행시켜야 하는 부분이 있다면 사용하면 좋을거같다.**
             t2.join();
         } catch (InterruptedException ie) {
             System.out.println(ie.toString());
