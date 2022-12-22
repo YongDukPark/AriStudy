3137d3ea3938f7aadbd7c62b0b0f6388fd68a03f
ParkYoungDuk <dydejrdms@naver.com>
2022-12-22 17:42:47 +0900

    FlowTest 설명문구 추가

--- src/chapter10_AWT/FlowTest.java
+++ src/chapter10_AWT/FlowTest.java
@@ -11,28 +11,31 @@ public class FlowTest {
         Frame f = new Frame("FlowLayout");
         Button b1, b2, b3, b4, b5;
         
+        //이쁜이 버튼들 만들어주기
         b1 = new Button("버튼1");
         b2 = new Button("버튼2");
         b3 = new Button("버튼3");
         b4 = new Button("버튼4");
         b5 = new Button("버튼5");
         
+        //x기능 넣어주기
         f.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e){
                 System.exit(0);
             }
         });
-        
+        //판넬 생성하기
         Panel p = new Panel();
-        
+        //p에 버튼들 넣어주기
         p.add(b1);
         p.add(b2);
         p.add(b3);
         p.add(b4);
         p.add(b5);
-        
+        //f에 생성시키기 p를 center에
         f.add(p, "Center");
         
+        //Frame에 size 셋팅하고 출력시키는 visible method
         f.setSize(400,100);
         f.setVisible(true);
     }
