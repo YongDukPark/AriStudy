f29802852d42e1e1622b217bb96be124e011401b
ParkYoungDuk <dydejrdms@naver.com>
2022-12-22 11:15:05 +0900

    Fram 예제 작성

--- src/chapter10_AWT/FrameTest.java
+++ src/chapter10_AWT/FrameTest.java
@@ -1,5 +1,33 @@
 package chapter10_AWT;
 
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.event.*;
+
 public class FrameTest {
-    
+    public static void main(String[] args) {
+        //여기서 생성자로 보내는 text는 타이틀 바에 표시될 제목이다.
+        //Frame f = new Frame("나는야 멋쟁이 Frame");
+        Frame f = new Frame();
+        //이런식으로도 title설정이 가능하다.
+        f.setTitle("나는야 멋쟁이 Frame");
+        //위에서 set 진행한 Title명을 출력할수 있다.
+        System.out.println(f.getTitle());
+        
+        //자 이벤트를 추가하는 메서드를 생성해 준다.
+        f.addWindowListener( new WindowAdapter(){
+            public void windowClosing(WindowEvent e) {
+                //여기 이벤트를 진행시키면 창이 닫힌다.
+                System.exit(0);
+            }
+        });
+        //프레임의 사이즈를 지정한다.
+        f.setSize(300, 300);
+        //프레임의 생성 위치를 지정해준다.
+        f.setLocation(100, 100);
+        //프레임의 배경색상을 지정한다.
+        f.setBackground(Color.yellow);
+        //프레임을 표시한다.
+        f.setVisible(true);
+    }
 }
--- src/chapter10_AWT/NewJFrame.java
+++ src/chapter10_AWT/NewJFrame.java
@@ -27,17 +27,37 @@ public class NewJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("jButton1");
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(105, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addContainerGap(167, Short.MAX_VALUE))
         );
 
         pack();
@@ -79,5 +99,8 @@ public class NewJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
